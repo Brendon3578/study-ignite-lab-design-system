5f79ad8730255176e2ad1205d2d8ac165000fbc5
Brendon Gomes <brendongomes40@gmail.com>
2022-10-17 00:39:48 -0300

    :bug: msw storybook github page error not fixed

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
+/*
+  OBS: no Github Pages o MSW apresenta um erro
+  (é possível vê-lo dentro do console, apontando que o script
+  "mockServiceWorker.js não foi encontrado")
+*/
 async function main() {
   if (process.env.NODE_ENV === "development") {
     if (window.location.pathname === "/study-ignite-lab-design-system") {
--- src/pages/SignIn/SignIn.stories.tsx
+++ src/pages/SignIn/SignIn.stories.tsx
@@ -2,7 +2,6 @@ import { Meta, StoryObj } from "@storybook/react";
 import { SignIn } from "./SignIn";
 import { within, userEvent, waitFor } from "@storybook/testing-library";
 import { expect } from "@storybook/jest";
-import { rest } from "msw";
 import { handlers } from "../../mocks/handlers";
 
 export default {
@@ -17,6 +16,11 @@ export default {
   },
 } as Meta;
 
+/*
+  OBS: no Github Pages o MSW apresenta um erro
+  (é possível vê-lo dentro do console, apontando que o script
+  "mockServiceWorker.js não foi encontrado")
+*/
 export const Default: StoryObj = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
